1aa8c14a06970e43f54cb60deb0cced9fef35394
Gautham495 <kinggautham495@gmail.com>
2023-04-18 23:52:07 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View} from 'react-native';
+import React from 'react';
+import Logo from '../logo.svg';
+
+const App = () => {
+  return (
+    <View>
+      <Logo height={45} width={45} />
+    </View>
+  );
+};
+
+export default App;
